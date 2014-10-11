9034e7eda64842735f325dfc450b9a1dbabf4f59
Marc Villacorta <marc.villacorta@gmail.com>
2014-10-11 22:15:40 +0200

    Adding cgitrc file

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,3 +1,10 @@
 class cgit::config inherits cgit {
 
+  file { '/etc/cgitrc':
+    ensure  => present,
+    content => template("${module_name}/cgitrc.erb"),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+  }
 }
